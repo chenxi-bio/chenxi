293d6d6ee07153517e5dcf9dac16a265559d78ba
chenxi-bio <zchx1120@163.com>
2022-01-17 16:05:53 +0800

    New background.

--- README.txt
+++ README.txt
@@ -1,10 +1,12 @@
-Miniport by HTML5 UP
+Alpha by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-A super simple responsive portfolio template. It's only a single page because hey, sometimes
-you don't need a half dozen pages to make your point.
+A clean, super minimal responsive template geared towards startups, app devs, and other
+dedicated folks working tirelessly to launch their products. Includes a landing page,
+generic page, contact page, and a page with a whole mess of pre-styled elements (something
+new I'm trying out). Sass sources are also included.
 
 Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
 you can use for pretty much whatever.
@@ -29,4 +31,5 @@ Credits:
 
 	Other:
 		jQuery (jquery.com)
+		Scrollex (github.com/ajlkn/jquery.scrollex)
 		Responsive Tools (github.com/ajlkn/responsive-tools)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Miniport by HTML5 UP
+	Alpha by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -8,14 +8,17 @@
 
 	var	$window = $(window),
 		$body = $('body'),
-		$nav = $('#nav');
+		$header = $('#header'),
+		$banner = $('#banner');
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			wide:      ( '1281px',  '1680px' ),
+			normal:    ( '981px',   '1280px' ),
+			narrow:    ( '737px',   '980px'  ),
+			narrower:  ( '737px',   '840px'  ),
+			mobile:    ( '481px',   '736px'  ),
+			mobilep:   ( null,      '480px'  )
 		});
 
 	// Play initial animations on page load.
@@ -25,10 +28,57 @@
 			}, 100);
 		});
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			alignment: 'right'
 		});
+
+	// NavPanel.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+	// Header.
+		if (!browser.mobile
+		&&	$header.hasClass('alt')
+		&&	$banner.length > 0) {
+
+			$window.on('load', function() {
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight(),
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt reveal'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			});
+
+		}
 
 })(jQuery);
